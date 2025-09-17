c34af2a3a61ba4d32d8e1a9f9e631a015e3ab770
emilyclam <emilychilam@gmail.com>
2025-09-17 15:07:23 -0700

    initial commit

---
+++ popup/popup.js
@@ -0,0 +1,21 @@
+function populatePokemonList() {
+  chrome.storage.local.get({ caughtPokemon: [] }, (data) => {
+    const caughtPokemon = data.caughtPokemon;
+    const list = document.getElementsByClassName('pokemonList')[0];
+    if (caughtPokemon.length == 0) {
+      const text = document.createElement("p");
+      text.textContent = "You haven't caught any pokemon yet. Time to start leetcoding!";
+      document.body.append(text);
+    }
+    for (let i = 0; i < caughtPokemon.length; i++) {
+      const sprite = document.createElement("img");
+      sprite.src = caughtPokemon[i].front;
+      sprite.className = 'sprite';
+      list.appendChild(sprite);
+      // later: add onClick to see more details
+      // or maybe in the tooltip?
+    }
+  });
+}
+
+populatePokemonList();
