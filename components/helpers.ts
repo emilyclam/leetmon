4d2938ff5335e3fd43a754160ddfe47d17117ab9
emilyclam <emilychilam@gmail.com>
2025-09-17 19:41:38 -0700

    added animation to catch pokemon !!

--- components/helpers.ts
+++ components/helpers.ts
@@ -37,35 +37,47 @@ export function throwPokeball() {
   pokeball.src = pokeballUrl;
   pokeball.className = 'pokeball';
   document.body.appendChild(pokeball);
+  const pokemon = document.getElementsByClassName('sprite')[0];
 
-  gsap.to(pokeball, {
+  let tl = gsap.timeline();
+  tl.to(pokeball, {
     duration: 0.8,
     motionPath: {
       path: [
         { x: 0, y: 0 },
-        { x: 200, y: -150 },
-        { x: 400, y: 0 }
+        { x: 100, y: -300 },
+        { x: 200, y: -100 }
       ],
       curviness: 1.5
     },
     rotation: 720,
-    ease: "power1.inOut",
-    onComplete: () => {
-      gsap.to(pokeball, {
-        y: "50",
-        duration: 0.5,
-        ease: "bounce.out",
-        onComplete: () => {
-          gsap.to(pokeball, {
-            delay: 0.3,
-            rotation: "+=40",
-            duration: 0.1,
-            yoyo: true,
-            repeat: 1,
-            ease: "power4.out"
-          })
-        }
-      });
-    }
-  });
+    ease: "power1.inOut"
+  })
+  .to(pokemon, {
+    scale: 0,
+    duration: 0.2
+  })
+  .to(pokeball, {
+    y: "-40",
+    duration: 0.5,
+    ease: "bounce.out"
+  })
+  .to(pokeball, {
+    delay: 0.6,
+    rotation: "+=40",
+    x: "+=5",
+    duration: 0.1,
+    yoyo: true,
+    repeat: 1,
+    ease: "power4.out"
+  })
+  .to(pokeball, {
+    delay: 0.8,
+    rotation: "-=40",
+    x: "-=5",
+    duration: 0.1,
+    yoyo: true,
+    repeat: 1,
+    ease: "power4.out"
+  })
 }
