e698834d6f46523daf1cb79f01521f5fee258efc
emilyclam <emilychilam@gmail.com>
2025-09-18 16:21:20 -0700

    updated extension title and description

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   manifest: {
+    name: "LeetMon",
     permissions: ['storage']
   },
 });
